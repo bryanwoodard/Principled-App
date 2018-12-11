0353a46d34b15f0eade613a4d81d1105077fe89c
Bryan Woodard <bwoodard40@gmail.com>
2018-12-10 20:56:06 -0800

    Homepage
    Adds Rudimentary structure for homepage

--- models/track.js
+++ models/track.js
@@ -1,27 +1,30 @@
 var mongoose = require('mongoose');
 
-var TrackSchema = new mongoose.Schema({
-  trackName: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  principle: {
-    type: Array,
-    required: true,
-    trim: true,
-  },
-  startTime: {
-    type: Number,
-    required: true,
-    trim: true
-  },
-  interval: {
-    type: Number,
-    required: true
-  }
-});
+// var TrackSchema = new mongoose.Schema({
+//   trackName: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     unique: true,
+//   },
+//   principle: {
+//     type: Array,
+//     required: true,
+//     trim: true,
+//   },
+//   startTime: {
+//     type: Number,
+//     required: true,
+//     trim: true
+//   },
+//   interval: {
+//     type: Number,
+//     required: true
+//   }
+// });
 
-var Track = mongoose.model('track', TrackSchema);
+// var Track = mongoose.model('0track', TrackSchema);
+
+//defining track as a placeholder until schema is built and db is purged
+var Track = "";
 module.exports = Track;
